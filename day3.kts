9299f1b84058f99a98c6fc32c1a2785c716782a2
Keith Playford <keith.playford@pobox.com>
2020-12-03 14:17:26 +0000

    Minor cleanups

--- day3.kts
+++ day3.kts
@@ -16,16 +16,6 @@ enum class Cell {
     TREE
 }
 
-data class Location(var x: Int, var y: Int)
-data class Slope(var deltax: Int, var deltay: Int)
-
-
-val map = mutableListOf<List<Cell>>();
-
-// x = 0, y = 0 is top left
-fun mapCell(x: Int, y: Int): Cell = map[y][x % map[0].size];
-fun mapCell(l: Location): Cell = mapCell(l.x, l.y);
-
 fun parseCell(c: Char): Cell {
     when (c) {
         '.' -> return Cell.EMPTY;
@@ -34,24 +24,33 @@ fun parseCell(c: Char): Cell {
     }
 }
 
-fun processSlope(deltax: Int, deltay: Int): Int {
+data class Location(var x: Int, var y: Int)
+data class Slope(var deltax: Int, var deltay: Int)
+
+val map = mutableListOf<List<Cell>>();
+
+// x = 0, y = 0 is top left
+fun mapCell(x: Int, y: Int): Cell = map[y][x % mapWidth()];
+fun mapCell(l: Location): Cell = mapCell(l.x, l.y);
+fun mapWidth() = map[0].size;
+fun mapHeight() = map.size;
+
+fun processSlope(slope: Slope): Int {
     val cursor = Location(0, 0);
-    var treeHits = 0;
+    var hits = 0;
 
-    while (true) {
-        if (mapCell(cursor) == Cell.TREE) ++treeHits;
-        cursor.x += deltax;
-        cursor.y += deltay;
+    do {
+        if (mapCell(cursor) == Cell.TREE) ++hits;
 
-        if (cursor.y >= map.size) break;
-    }
+        cursor.x += slope.deltax; cursor.y += slope.deltay;
+    } while (cursor.y < mapHeight())
 
-    return treeHits;
+    return hits;
 }
 
 // Answer: 36
 fun processPart1() {
-    val hits = processSlope(3, 2);
+    val hits = processSlope(Slope(3, 2));
     println("Hits $hits");
 }
 
@@ -62,7 +61,7 @@ fun processPart2() {
     var product: Long = 1L;
 
     for (slope in slopes) {
-        val hits = processSlope(slope.deltax, slope.deltay);
+        val hits = processSlope(slope);
         // println("Hits $hits");
         product *= hits;
     }
