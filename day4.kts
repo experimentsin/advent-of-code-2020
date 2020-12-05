4c3d13b65518879161e64ca9f261461423ef5616
Keith Playford <keith.playford@pobox.com>
2020-12-05 14:41:03 +0000

    Some light Kotlin idiomatic cleanup

--- day4.kts
+++ day4.kts
@@ -1,64 +1,64 @@
 fun loadPassports(): List<String> {
-    val passports = mutableListOf<String>();
-    var passportLines = "";
+    val passports = mutableListOf<String>()
+    var passportLines = ""
 
     fun commitPassport() {
         if (!passportLines.isEmpty()) {
-            passports.add(passportLines);
-            passportLines = "";            
+            passports.add(passportLines)
+            passportLines = ""   
         }
     }
 
     while (true) {
-        val line = readLine();
+        val line = readLine()
 
         // End of file
         if (line == null) {
-            commitPassport();
-            return passports;
+            commitPassport()
+            return passports
         }
 
-        val trimmed = line.trim();
+        val trimmed = line.trim()
 
         // Blank line
         if (trimmed.isEmpty()) {
-            commitPassport();
-            continue;
+            commitPassport()
+            continue
         }
 
         // Line with content
-        if (!passportLines.isEmpty()) passportLines += " ";
-        passportLines += line.trim();
+        if (!passportLines.isEmpty()) passportLines += " "
+        passportLines += line.trim()
     }
 }
 
-data class Passport (val fields: HashMap<String, String> = hashMapOf<String, String>());
+data class Passport (val fields: HashMap<String, String> = hashMapOf<String, String>())
 
 fun compilePassports(texts: List<String>): List<Passport> {
     fun compilePassport(text: String): Passport {
-        val pp = Passport();
-        val fieldTexts = text.split(" ");
+        val pp = Passport()
+        val fieldTexts = text.split(" ")
         for (fieldText in fieldTexts) {
-            val (key, value) = fieldText.split(":");
-            pp.fields[key] = value;
+            val (key, value) = fieldText.split(":")
+            pp.fields[key] = value
         }
-        return pp;
+        return pp
     }
 
-    return texts.map(::compilePassport);
+    return texts.map(::compilePassport)
 }
 
-val requiredFieldsPart1 = listOf("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid");
+val requiredFieldsPart1 = listOf("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid")
 
 fun isValidPassportPart1(pp: Passport): Boolean {
-    // println("isValidPassport: $pp");
+    // println("isValidPassport: $pp")
     for (field in requiredFieldsPart1) {
         if (pp.fields[field] == null) {
-            // println("Missing $field");
-            return false;
+            // println("Missing $field")
+            return false
         }
     }
-    return true;
+    return true
 }
 
 /*
@@ -69,64 +69,64 @@ fun isValidPassportPart1(pp: Passport): Boolean {
 
 abstract class FieldSpec(val name: String, val pattern: String) {
 
-    val regex = pattern.toRegex();
+    val regex = pattern.toRegex()
 
     fun validate(input: String): Boolean {
-        val match = regex.matchEntire(input);
-        if (match === null) return false;
+        val match = regex.matchEntire(input)
+        if (match === null) return false
 
-        val result = validateMatch(match.destructured);
-        return result;
+        val result = validateMatch(match.destructured)
+        return result
     }
 
-    fun inRange(trial: Int, low: Int, high: Int): Boolean = low <= trial && trial <= high;
+    fun inRange(trial: Int, low: Int, high: Int): Boolean = low <= trial && trial <= high
 
-    abstract fun validateMatch(match: MatchResult.Destructured): Boolean;
+    abstract fun validateMatch(match: MatchResult.Destructured): Boolean
 }
 
 object ByrSpec : FieldSpec("byr", """(\d\d\d\d)""") {
     override fun validateMatch(match: MatchResult.Destructured): Boolean {
-        val (year) = match;
-        return inRange(year.toInt(), 1920, 2002);
+        val (year) = match
+        return year.toInt() in 1920..2002
      }
 }
 
 object IyrSpec : FieldSpec("iyr", """(\d\d\d\d)""") {
     override fun validateMatch(match: MatchResult.Destructured): Boolean {
-        val (year) = match;
-        return inRange(year.toInt(), 2010, 2020);
+        val (year) = match
+        return year.toInt() in 2010..2020
      }
 }
 
 object EyrSpec : FieldSpec("eyr", """(\d\d\d\d)""") {
     override fun validateMatch(match: MatchResult.Destructured): Boolean {
-        val (year) = match;
-        return inRange(year.toInt(), 2020, 2030);
+        val (year) = match
+        return year.toInt() in 2020..2030
      }
 }
 
 object HgtSpec : FieldSpec("hgt", """(\d+)(.+)""") {
     override fun validateMatch(match: MatchResult.Destructured): Boolean {
-        val (height, unit) = match;
-        val iheight = height.toInt();
+        val (height, unit) = match
+        val iheight = height.toInt()
         when (unit) {
-            "cm" -> return inRange(iheight, 150, 193)
-            "in" -> return inRange(iheight, 59, 76)
-            else -> return false;
+            "cm" -> return iheight in 150..193
+            "in" -> return iheight in 59..76
+            else -> return false
         }
     }
 }
 
 object HclSpec : FieldSpec("hcl", """#([a-f0-9]*)""") {
     override fun validateMatch(match: MatchResult.Destructured): Boolean {
-        val (rgb) = match;
-        return rgb.length == 6;    
+        val (rgb) = match
+        return rgb.length == 6    
     }
 }
 
 object EclSpec : FieldSpec("ecl", """(.*)""") {
     override fun validateMatch(match: MatchResult.Destructured): Boolean {
-        val (col) = match;
+        val (col) = match
         when (col) {
             "amb", "blu", "brn", "gry", "grn", "hzl", "oth" -> return true
             else -> return false
@@ -135,9 +135,7 @@ object EclSpec : FieldSpec("ecl", """(.*)""") {
 }
 
 object PidSpec : FieldSpec("pid", """(\d\d\d\d\d\d\d\d\d)""") {
-    override fun validateMatch(match: MatchResult.Destructured): Boolean {
-        return true;
-    }
+    override fun validateMatch(match: MatchResult.Destructured): Boolean = true
 }
 
 val requiredFieldsPart2 = listOf(ByrSpec, IyrSpec, EyrSpec, HgtSpec, HclSpec, EclSpec, PidSpec)
@@ -145,10 +143,10 @@ val requiredFieldsPart2 = listOf(ByrSpec, IyrSpec, EyrSpec, HgtSpec, HclSpec, Ec
 fun testSpecs() {
 
     fun checkValid(spec: FieldSpec, input: String) {
-        if (!spec.validate(input)) throw Error("checkValid failed $spec $input");
+        if (!spec.validate(input)) throw Error("checkValid failed $spec $input")
     }
     fun checkInvalid(spec: FieldSpec, input: String) {
-        if (spec.validate(input)) throw Error("checkInvalid failed $spec $input");
+        if (spec.validate(input)) throw Error("checkInvalid failed $spec $input")
     }
     fun checkAllValid(spec: FieldSpec, vararg input: String) {
         input.forEach({ checkValid(spec, it)})
@@ -157,61 +155,62 @@ fun testSpecs() {
         input.forEach({ checkInvalid(spec, it)})
     }
 
-    checkAllValid(ByrSpec, "1920", "2002");
-    checkAllInvalid(ByrSpec, "1919", "2003", "ABCD", "200", "20000");
+    checkAllValid(ByrSpec, "1920", "2002")
+    checkAllInvalid(ByrSpec, "1919", "2003", "ABCD", "200", "20000")
 
-    checkAllValid(IyrSpec, "2010", "2020");
-    checkAllInvalid(IyrSpec, "2009", "2021", "ABCD", "200", "20000");
+    checkAllValid(IyrSpec, "2010", "2020")
+    checkAllInvalid(IyrSpec, "2009", "2021", "ABCD", "200", "20000")
 
-    checkAllValid(EyrSpec, "2020", "2030");
-    checkAllInvalid(EyrSpec, "2019", "2031", "ABCD", "200", "20000");
+    checkAllValid(EyrSpec, "2020", "2030")
+    checkAllInvalid(EyrSpec, "2019", "2031", "ABCD", "200", "20000")
 
-    checkAllValid(HgtSpec, "150cm", "193cm", "59in", "76in");
-    checkAllInvalid(HgtSpec, "149cm", "194cm", "58in", "77in", "100c", "100cmx", "100i", "100inx", "100ab");
+    checkAllValid(HgtSpec, "150cm", "193cm", "59in", "76in")
+    checkAllInvalid(HgtSpec, "149cm", "194cm", "58in", "77in", "100c", "100cmx", "100i", "100inx", "100ab")
 
-    checkAllValid(HclSpec, "#1a2b3c", "#a1b1c1", "#123456", "#789012", "#abcdef");
-    checkAllInvalid(HclSpec, "#1a", "#1a2b", "#1a2b3c4e", "#1x2y3z", "1a2b3c");
+    checkAllValid(HclSpec, "#1a2b3c", "#a1b1c1", "#123456", "#789012", "#abcdef")
+    checkAllInvalid(HclSpec, "#1a", "#1a2b", "#1a2b3c4e", "#1x2y3z", "1a2b3c")
 
-    checkAllValid(EclSpec, "amb", "blu", "brn", "gry", "grn", "hzl", "oth");
-    checkAllInvalid(EclSpec, "", "am", "ambblue", "ambo", " amb ");
+    checkAllValid(EclSpec, "amb", "blu", "brn", "gry", "grn", "hzl", "oth")
+    checkAllInvalid(EclSpec, "", "am", "ambblue", "ambo", " amb ")
 
-    checkAllValid(PidSpec, "123456789", "012345678");
-    checkAllInvalid(PidSpec, "12345678", "1234567890", "abcdefghi");
+    checkAllValid(PidSpec, "123456789", "012345678")
+    checkAllInvalid(PidSpec, "12345678", "1234567890", "abcdefghi")
 
 }
 
 fun isValidPassportPart2(pp: Passport): Boolean {
     for (field in requiredFieldsPart2) {
-        if (pp.fields[field.name] == null) {
-            // println("Missing $field");
-            return false;
+        val fieldData = pp.fields[field.name]
+        if (fieldData == null) {
+            // println("Missing $field")
+            return false
         }
-        if (!field.validate(pp.fields[field.name]!!)) {
-            // println("Validation failed $field");
-            return false;          
+        if (!field.validate(fieldData)) {
+            // println("Validation failed $field")
+            return false          
         }
-        // println("Validation succeeded $field");
+        // println("Validation succeeded $field")
     }
-    return true;
+    return true
 }
 
 // Answer: 226
 fun processPart1() {
-    // println("Passports $passports");
-    val valid = passports.count({ isValidPassportPart1(it) });
-    println("Valid $valid");
+    // println("Passports $passports")
+    val valid = passports.count(::isValidPassportPart1)
+    println("Valid $valid")
 }
 
 // Answer: 160
 fun processPart2() {
-    // println("Passports $passports");
-    val valid = passports.count({ isValidPassportPart2(it) });
-    println("Valid $valid");
+    // println("Passports $passports")
+    val valid = passports.count(::isValidPassportPart2)
+    println("Valid $valid")
 }
 
-val passportTexts = loadPassports();
-val passports = compilePassports(passportTexts);
+val passportTexts = loadPassports()
+val passports = compilePassports(passportTexts)
 
-processPart1();
-testSpecs();
-processPart2();
+processPart1()
+testSpecs()
+processPart2()
--- day5.kts
+++ day5.kts
@@ -1,71 +1,57 @@
 fun parseInput(): List<String> {
-    val lines = mutableListOf<String>();
+    val lines = mutableListOf<String>()
     while (true) {
-        val line = readLine();
-        if (line == null) break;
+        val line = readLine()
+        if (line == null) break
 
-        lines.add(line);
+        lines.add(line)
     }
-    return lines;
+    return lines
 }
 
-fun binaryPartition(direction: String, index: Int, low: Int, high: Int): Int {
-    println("dir $direction $index $low $high")
-    if (index >= direction.length) {
-        println("Low/high $low $high");
-        return low;
+fun binaryPartition(codes: String, isLowCode: (Char) -> Boolean, index: Int, low: Int, high: Int): Int {
+    // println("dir $codes $index $low $high")
+    if (index >= codes.length) {
+        // println("Low/high $low $high")
+        return low
     }
 
-    val mid = (low + high) / 2;
-    if (isGoLow(direction[index])) {
-        return binaryPartition(direction, index + 1, low, mid);
+    val mid = (low + high) / 2
+    if (isLowCode(codes[index])) {
+        return binaryPartition(codes, isLowCode, index + 1, low, mid)
     } else {
-        return binaryPartition(direction, index + 1, mid + 1, high);
+        return binaryPartition(codes, isLowCode, index + 1, mid + 1, high)
     }
-}
-
-fun isGoLow(c: Char): Boolean {
-    return c == 'F' || c == 'L';
 }
 
 fun processCardPart1(card: String): Int {
-    val fb = card.substring(0, 7);
-    val lr = card.substring(7, 10);
+    val fb = card.substring(0, 7)
+    val lr = card.substring(7, 10)
 
-    val row = binaryPartition(fb, 0, 0, 127);
-    val col = binaryPartition(lr, 0, 0, 7);    
+    val row = binaryPartition(fb, { it == 'F' }, 0, 0, 127)
+    val col = binaryPartition(lr, { it == 'L' }, 0, 0, 7)
 
-    var id = row * 8 + col;
-    println("For $card row $row col $col $id");
+    var id = row * 8 + col
+    // println("For $card row $row col $col $id")
 
-    return id;
+    return id
 }
 
 // Answer: 928 @ 23 mins
 fun processPart1() {
-    var maxId = -1;
-    for (card in data) {
-        val id = processCardPart1(card);
-        maxId = maxOf(id, maxId);
-    }
-    println("Max $maxId");
+    val maxId = data.map(::processCardPart1).maxOrNull()
+    println("Max $maxId")
 }
 
 // Answer: 610 @ 30 mins total
 fun processPart2() {
-    val ids = data.map(::processCardPart1).toMutableList();
+    val ids = data.map(::processCardPart1).toMutableList()
     ids.sort();
-
-    var last = -1;
-    for (id in ids) {
-        if (last >= 0 && last + 1 != id) {
-            println("Missing: ${id - 1}");
-        }
-        last = id;
-    }
-    println("sorted: $ids")
+    val range = ids.first()..ids.last()
+    val diff = range.subtract(ids)
+    println("diff $diff")
 }
 
-val data = parseInput();
-processPart1();
-processPart2();
+val data = parseInput()
+processPart1()
+processPart2()
