12958adfe0eb617499ae0715e3f6282af155bf42
Keith Playford <keith.playford@pobox.com>
2020-12-17 23:44:46 +0000

    Questionable Kotlin experiments

--- day17.kts
+++ day17.kts
@@ -16,17 +16,135 @@ fun <T>parseLinesAsGridAndMap(mapper: (c: Char) -> T): List<List<T>> {
     return parseLines().map({ it.map(mapper).toMutableList() })
 }
 
+// A defaulting hash map
+
+// You can achieve something similar in Kotlin by delegation as outlined in the commend below
+// but HashMap is an open class so we can derive and override directly
+
+/*
+class Map4X<K, T>(val default: T, val m: HashMap<K, T> = hashMapOf<K, T>()) : MutableMap<K, T> by m {
+    constructor(tocopy: Map4X<K, T>): this(tocopy.default, HashMap<K, T>(tocopy.m)) 
+
+    operator override fun get(key: K) = m.get(key)
+}
+*/
+
+class DefaultingHashMap<K, T> : HashMap<K, T> {
+    val default: T
+
+    constructor(default: T) { this.default = default }
+    constructor(default: T, tocopy: DefaultingHashMap<K, T>) : super(tocopy) { this.default = default }
+    constructor(tocopy: DefaultingHashMap<K, T>): this(tocopy.default, tocopy)
+
+    operator override fun get(key: K) = super.get(key) ?: default
+}
+
+// How would we define a completely generic vector class?
+
+// Common axis names for convenience
+
+val (_x, _y, _z, _w) = listOf(0, 1, 2, 3)
+
+
+// Turns out Kotlin can't do generics over different number types so
+// we have to choose a concrete type
+
+data class Vec(val values: List<Long>) : List<Long> by values {
+    constructor(vararg value: Long): this(value.toList())
+    constructor(vararg value: Int): this(value.toList().map { it.toLong() })
+
+    fun isCompatible(v: Vec) = this.size == v.size
+
+    fun assertCompatible(v: Vec) {
+        if (!isCompatible(v)) throw Error("Incompatible Vecs ${this} ${v}")
+    }
+
+    override fun toString() = "V" + values.toString()
+
+    // It's a bit naughty exposing List<Long> as an interface parent
+    // then defining static overrides for + and - but the other List
+    // ops will be kind of useful
+
+    operator fun plus(v: Vec): Vec {
+        assertCompatible(v)
+        // return Vec(this.zip(v, { a, b -> a + b }))
+        return Vec(MutableList(this.size) { i -> this[i] + v[i] })
+    }
+
+    operator fun minus(v: Vec): Vec {
+        assertCompatible(v)
+        return Vec(this.zip(v, { a, b -> a - b }))
+    }
+
+}
+
+// Disambiguation overrides 
+
+operator fun List<Vec>.plus(v: Vec): List<Vec> {
+    val extended = this.toMutableList()
+    extended.add(v)
+    return extended
+}
+
+operator fun List<Vec>.minus(v: Vec): List<Vec> {
+    val removed = this.toMutableList()
+    removed.remove(v)
+    return removed
+}
+
+fun allNeighbours(v: Vec): List<Vec> {
+    return allNeighbours(v.size).map { dv -> v + dv }
+}
+
+fun nonDiagonalNeighbours(v: Vec): List<Vec> {
+    return nonDiagonalNeighbours(v.size).map { dv -> v + dv }
+}
+
+val allNeighboursCache = hashMapOf<Int, List<Vec>>()
+
+fun allNeighbours(dimensions: Int): List<Vec> {
+    val cached = allNeighboursCache[dimensions]
+    if (cached !== null) {
+        return cached
+    }
+
+    val nbs = mutableListOf<Vec>()
+    fun walk(i: Int, sofar: List<Long>) {
+        if (i >= dimensions) {
+            if (sofar.count { it == 0L } != dimensions) {
+                nbs.add(Vec(sofar))
+            }
+            return
+        }
+
+        for (axisDelta in -1L..1L) {
+            walk(i + 1, sofar + axisDelta)
+        }
+        
+    }
+    walk(0, listOf<Long>())
+
+    allNeighboursCache[dimensions] = nbs
+    return nbs
+}
+
+val allNonDiagonalNeighboursCache = hashMapOf<Int, List<Vec>>()
+
+fun nonDiagonalNeighbours(dimensions: Int): List<Vec> {
+    val cached = allNonDiagonalNeighboursCache[dimensions]
+    if (cached !== null) return cached
+
+    val result = allNeighbours(dimensions).filter { it.count { it != 0L } == 1 }
+    allNonDiagonalNeighboursCache[dimensions] = result
+    
+    return result
+}
+
 /// Solution
 
 enum class Cube(val display: Char) { ACTIVE('#'), INACTIVE('.') }
 
-fun parseCell(c: Char): Cube {
-    return when (c) {
-        '#' -> Cube.ACTIVE
-        '.' -> Cube.INACTIVE
-        else -> throw Error("Error bad input char ${c}")
-    }
-}
+fun parseCube(c: Char): Cube = Cube.values().find { it.display == c } ?: throw Error("Bad input char ${c}")
 
 // x > and y ^ and z away
 
@@ -145,76 +263,22 @@ fun processPart1() {
     println("Part 1 answer - ${count} cubes active after 6 iterations")
 }
 
-// There is an obvious generalisation to N dimensions here that also allows logic sharing
-// e.g. having a hash map representation of vectors, but I just did the lazy thing: copy,
-// paste, and tweak
-
-data class Vec4(val x: Int, val y: Int, val z: Int, val w: Int)
-
-operator fun Vec4.plus(v: Vec4): Vec4 = Vec4(x + v.x, y + v.y, z + v.z, w + v.w)
-
-fun neighbourVecs4(): List<Vec4> {
-    val vecs = mutableListOf<Vec4>()
-    for (x in -1..1) {
-        for (y in -1..1) {
-            for (z in -1..1) {
-                for (w in -1..1) {
-                    if (x == 0 &&  y == 0 && z == 0 && w == 0) continue
-                    vecs.add(Vec4(x, y, z, w))
-                }
-            }
-        }
-    }
-
-    if (vecs.size != 3*3*3*3 - 1) throw Error("Bad neighbourVecs 4D")
-
-    return vecs
-}
-
-val NEIGHBOUR_VECS4 = neighbourVecs4()
-
-fun neighbours4(v: Vec4): List<Vec4> = NEIGHBOUR_VECS4.map { dv -> v + dv }
-
-
-// A defaulting hash map
-
-// You can achieve something similar in Kotlin by delegation as outlined in the commend below
-// but HashMap is an open class so we can derive and override directly
-
-/*
-class Map4X<K, T>(val default: T, val m: HashMap<K, T> = hashMapOf<K, T>()) : MutableMap<K, T> by m {
-    constructor(tocopy: Map4X<K, T>): this(tocopy.default, HashMap<K, T>(tocopy.m)) 
-
-    operator override fun get(key: K) = m.get(key)
-}
-*/
-
-class DefaultingHashMap<K, T> : HashMap<K, T> {
-    val default: T
-
-    constructor(default: T) { this.default = default }
-    constructor(default: T, tocopy: DefaultingHashMap<K, T>) : super(tocopy) { this.default = default }
-    constructor(tocopy: DefaultingHashMap<K, T>): this(tocopy.default, tocopy)
-
-    operator override fun get(key: K) = super.get(key) ?: default
-}
-
-typealias Map4 = DefaultingHashMap<Vec4, Cube>
+typealias Map4 = DefaultingHashMap<Vec, Cube>
 
 // Short of full N-dimensional generality, we could have used generics / functions to
 // share the lifecycle rule logic and basic algorithm
 
-fun runCycle4(m: Map4): Map4 {
-    val nextm = Map4(m)
+fun runCycle4(m: DefaultingHashMap<Vec, Cube>): DefaultingHashMap<Vec, Cube> {
+    val nextm = DefaultingHashMap<Vec, Cube>(m)
 
     for ((ov, _) in m) {
         // There's redundant calculation because of lazy windowing here but
         // it doesn't matter
-        val ovnbVecs = neighbours4(ov) + ov
+        val ovnbVecs = allNeighbours(ov) + ov
 
         for (v in ovnbVecs) {
             val cube = m[v] 
-            val nbActive = neighbours4(v).count { m[it] === Cube.ACTIVE }
+            val nbActive = allNeighbours(v).count { m[it] === Cube.ACTIVE }
             when (cube) {
                 Cube.ACTIVE -> {
                     if (nbActive !in 2..3) nextm[v] = Cube.INACTIVE
@@ -231,13 +295,11 @@ fun runCycle4(m: Map4): Map4 {
 
 // Answer - 2240
 fun processPart2() {
-    println("NVs ${NEIGHBOUR_VECS4}")
-
     val map = Map4(Cube.INACTIVE)
 
     for ((i, row) in data.withIndex()) {
         for ((j, cell) in row.withIndex()) {
-            val v = Vec4(j, -i, 0, 0)
+            val v = Vec(j, -i, 0, 0)
             map[v] = cell
         }
     }
@@ -252,6 +314,6 @@ fun processPart2() {
     println("Part 2 answer - ${count} cubes active after 6 iterations")
 }
 
-val data = parseLinesAsGridAndMap(::parseCell)
+val data = parseLinesAsGridAndMap(::parseCube)
 processPart1()
 processPart2()
